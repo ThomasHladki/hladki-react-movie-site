bb2eaa38797eeaa2c1ee7eab41f42f39086c0a5b
Thomas Hladki <tomhladki@icloud.com>
2022-11-17 17:01:37 -0500

    Further work on movie app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import SearchIcon from './search.svg';
+
 //key 6239be83
+const API_URL='http://www.omdbapi.com?apikey=6239be83';
+const movie1 = {
+    "Title": "Amazing Spiderman Syndrome",
+    "Year": "2012",
+    "imdbID": "tt2586634",
+    "Type": "movie",
+    "Poster": "N/A"
+}
+
 
 function App() {
+  
+  const searchMovies= async (title)=>{
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+  }
+  useEffect(()=>{
+    searchMovies('Spiderman');
+  }, []);
+
   return (
-    <div className="App">
-      <h1>App</h1>
-       
+    <div className="app"> 
+      <h1>MovieLand</h1>
+      <div className='search'>
+        <input placeholder='Search For Movies' value='Superman' 
+        onChange={()=>{
+
+        }}
+        />
+        <img src = {SearchIcon} alt='search' onClick={()=>{
+
+        }}
+        />
+      </div>
+      <div className='container'>
+        
+      </div>
     </div>
   );
 }
